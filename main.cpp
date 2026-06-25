2d876ab5970d9e61e1e817f848e98ffe0953d832
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-06-25 02:51:25 +0100

    edit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ double calculate(double a, double b, char op);
 int main()
 {
   cout << "This is a C++ calculator program" << endl;
+  cout << "------------- START -------------" << endl;
 
   char operation;
   cout << "Choose the operation you want from (+ - * /) : ";
@@ -21,14 +22,19 @@ int main()
 
   double result = calculate(first_number, second_number, operation);
 
-  if (result == 0 && second_number == 0){
+  if (result == 0 && operation == '/' && second_number == 0){
     cout << "Error : You can't divide by 0" << endl;
   }
   else if (result == 0 && (operation != '+' || operation != '-' || operation != '*' || operation != '/')){
     cout << "Error : Invalid operation" << endl;
   }
   else{
-    cout << "The result is : " << result << endl;
+    if (operation == '+' || operation == '-' || operation == '/'){
+    cout << "The result of " << first_number << " " << operation << " " << second_number << " is : " << result << endl;
+    } else if (operation == '*'){
+      cout << "The result of " << first_number << " x " << second_number << " is : " << result << endl;
+    }
   }
+  cout << "------------- END -------------" << endl;
   return 0;
 }
